1a6db03b2de287841700779227526e4e19edea92
ludacirs <ludacirs@naver.com>
2020-10-17 17:00:29 +0900

    Add fail, pass functions
    유효하지 않은 값 입력시 실패 메시지와 효과, 성공했을 때 효과 추가

--- validity-check/script.js
+++ validity-check/script.js
@@ -6,6 +6,20 @@
     const password = document.querySelector('#password');
     const password_check = document.querySelector('#password-check');
 
+    //값 입력이 잘 되었을 때
+    function pass(elem, str) {
+        elem.className = str;
+
+        //실패 메시지 숨기기
+        elem.parentNode.childNodes[5].style.visibility = 'hidden';
+    }
+    //값 입력이 실패 했을 때
+    function fail(elem, str){
+        elem.className = str;
+
+        //실패 메시지 띄우기
+        elem.parentNode.childNodes[5].style.visibility = 'visible';
+    }
 
     function valueCheck(u, email, pn, p1, p2) {
         let result = false;
@@ -16,38 +30,45 @@
     }
 
     function lengthCheck(u, pn, p1) {
-        if((u.value.length>4&&u.value.length<10)){
-            console.log('아이디 길이는 4글자 이상 10글자 이하');
-        }
+        //4글자 이상 10글지 이하 검사
+        if(!(u.value.length>=4&&u.value.length<=10)){
+            fail(u, 'fail');
+        } else pass(u, 'pass');
 
+        //11자리 핸드폰 번호 검사
         if(pn.value.length!==11){
-            console.log('핸드폰 번호는 11자리로 입력해주세요');
-        }
+            fail(pn, 'fail');
+        } else pass(pn, 'pass');
 
+        //8자리 이상 비밀번호 검사
         if(!(p1.value.length>=8)){
-            console.log('비밀번호는 8자리 이상입니다');
-        }
+            fail(p1, 'fail');
+        } else pass(p1, 'pass');
     }
 
 
     function emailCheck(email) {
-        const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        //이메일 정규식
+        const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
         if(!regEmail.exec(email.value)){
-            console.log('올바르지않은이메일형식');
-        }
+            fail(email, 'fail');
+        } else pass(email, 'pass');
     }
 
     function phoneNumberCheck(phone_number) {
+        //헨드폰 번호 정규식
         const regPhoneNumber = /^(010)(\d){8}/;
+
         if(!regPhoneNumber.exec(phone_number.value)){
-            console.log('번호는-를뺸 숫자만 입력해라');
-        }
+            fail(phone_number, 'fail');
+        }else pass(phone_number, 'pass');
     }
 
     function passwordSame(p1, p2) {
         if(p1.value!==p2.value){
-            console.log('비밀번호 값이 일치하지않음');
-        }
+            fail(p2, 'fail');
+        } else pass(p2, 'pass');
     }
 
 
@@ -56,7 +77,7 @@
         //이벤트 취소
         e.preventDefault();
 
-        //모든 input들의 값이 입력되었는지 확인
+        //모든 input 들의 값이 입력되었는지 확인
         if(valueCheck(username, email, phone_number, password, password_check)){
 
             //길이 확인
